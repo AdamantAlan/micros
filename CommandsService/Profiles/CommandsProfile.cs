b46c2c535c76acb665a56264476a87e1301f3cbf
Dmitriy Myagkov <myagkow.dima1337@gmail.com>
2021-10-17 17:41:14 +0500

    add dtos

---
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CommandsService.Dtos;
+using CommandsService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsService.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<Command, CommandReadDto>();
+        }
+    }
+}
